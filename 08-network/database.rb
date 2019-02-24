c3c46925bcf6f895f13bbd19df27a195f072a657
kenji.suzuki <kenji.suzuki.1984@gmail.com>
2019-02-24 23:47:03 +0900

    fix sider

--- 08-network/database.rb
+++ 08-network/database.rb
@@ -2,7 +2,7 @@ require 'redis'
 
 class Database
   def initialize
-    @redis = Redis.new(host: ENV['REDIS_HOST'], port: 6379, db: 08)
+    @redis = Redis.new(host: ENV['REDIS_HOST'], port: 6379, db: 8)
   end
 
   def save(key, data)
