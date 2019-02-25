9205f7453bccab9d3a328d5e08b83fb887b19266
kenji.suzuki <kenji.suzuki.1984@gmail.com>
2019-02-25 23:57:41 +0900

    update ８

--- 08-network/http_server.rb
+++ 08-network/http_server.rb
@@ -11,6 +11,9 @@ server.mount('/wallet', WalletServlet)
 server.mount('/genesis_block', GenesisBlockServlet)
 server.mount('/blockchain', BlockchainServlet)
 server.mount('/update_blockchain', UpdateBlockchainServlet)
+server.mount('/pay', PayServlet)
+server.mount('/transaction', TransactionServlet)
+server.mount('/start_pow', StartPowServlet)
 
 Signal.trap('INT'){server.shutdown}
 server.start
--- 08-network/servlets.rb
+++ 08-network/servlets.rb
@@ -7,7 +7,10 @@ class WalletServlet < WEBrick::HTTPServlet::AbstractServlet
     wallet = Wallet.new
     wallet.load
     if wallet.public_key
-      res.body = wallet.address
+      transactions = Transactions.new
+      transactions.load_all
+      balance = transactions.balance(wallet.address)
+      res.body = wallet.address + " " + balance.to_s
       res.status = 200
     else
       res.status = 404
@@ -58,12 +61,18 @@ end
 
 class UpdateBlockchainServlet < WEBrick::HTTPServlet::AbstractServlet
   def do_POST(req, res)
-    api_res = Faraday.get "http://" + ENV['NODE1'] + ":8000/blockchain", {key: "last_hash"}
+    host = req.query['node'] || ENV['NODE1']
+    api_res = Faraday.get "http://" + host + ":8000/blockchain", {key: "last_hash"}
     last_block = Marshal.load(api_res.body)
     last_hash = last_block.hash
 
     db = Database.new
+    blockchain = Blockchain.new
     unless db.exist_in_local?(last_hash)
+      unless blockchain.is_genesis_block(last_block)
+        pow = ProofOfWork.new(last_block)
+        return unless pow.validate
+      end
       db.save('last_hash', last_hash)
       db.save(last_hash, last_block)
     else
@@ -77,9 +86,63 @@ class UpdateBlockchainServlet < WEBrick::HTTPServlet::AbstractServlet
       if db.exist_in_local?(block.hash)
         break
       else
+        unless blockchain.is_genesis_block(block)
+          pow = ProofOfWork.new(block)
+          return unless pow.validate
+        end
         db.save(block.hash, block)
         prev_block_hash = block.prev_block_hash
       end
+    end
+    transactions = Transactions.new
+    transactions.delete_mem_pool
+  end
+end
+
+class PayServlet < WEBrick::HTTPServlet::AbstractServlet
+  def do_POST(req, res)
+    to = req.query['to']
+    amount = req.query['amount']
+
+    wallet = Wallet.new
+    wallet.load
+    new_transaction = wallet.pay(to, 10)
+    transactions = Transactions.new
+    if new_transaction.is_valid?
+      transactions.add_to_mem_pool new_transaction
+    end
+
+    dumped_new_transaction = Marshal.dump(new_transaction)
+    Faraday.post "http://" + ENV['NODE1'] + ":8000/transaction", {transaction: dumped_new_transaction}
+    Faraday.post "http://" + ENV['NODE2'] + ":8000/transaction", {transaction: dumped_new_transaction}
+  end
+end
+
+class TransactionServlet < WEBrick::HTTPServlet::AbstractServlet
+  def do_POST(req, res)
+    transaction = Marshal.load(req.query['transaction'])
+
+    transactions = Transactions.new
+    if transaction.is_valid?
+      transactions.add_to_mem_pool transaction
+    end
+  end
+end
+
+class StartPowServlet < WEBrick::HTTPServlet::AbstractServlet
+  def do_POST(req, res)
+    transactions = Transactions.new
+    transactions.load_all
+
+    if transactions.mem_pool.count > 0
+      blockchain = Blockchain.new
+      wallet = Wallet.new
+      wallet.load
+      blockchain.create_block(transactions.mem_pool, wallet.address)
+      transactions.delete_mem_pool
+
+      Faraday.post "http://" + ENV['NODE1'] + ":8000/update_blockchain", {node: ENV['ME']}
+      Faraday.post "http://" + ENV['NODE2'] + ":8000/update_blockchain", {node: ENV['ME']}
     end
   end
 end
